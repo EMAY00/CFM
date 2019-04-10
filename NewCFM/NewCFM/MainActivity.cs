3a600bc1bd7f0c7961c0450a039584341c918161
ElennMayAdeliine <33444865+ElennMayAdeliine@users.noreply.github.com>
2019-04-10 22:13:33 +0200

    10.04.2019
    repaired

---
+++ NewCFM/NewCFM/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+using Android.Provider;
+using Android.Graphics;
+
+namespace NewCFM
+{
+    [Activity(Label = "NewCFM", MainLauncher = true)]
+    public class MainActivity :Activity
+    {
+        int count = 1;
+        ImageView imageView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var btnCamera = FindViewById<Button>(Resource.Id.btnCamera);
+            imageView = FindViewById<ImageView>(Resource.Id.imageView);
+
+            btnCamera.Click += BtnCamera_Click;
+
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Bitmap bitmap = (Bitmap)data.Extras.Get("data");
+            imageView.SetImageBitmap(bitmap);
+
+        }
+
+        private void BtnCamera_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
+
+        }
+    }
+}
